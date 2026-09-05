f8d6ad697df612974b5c3d2928a302140cc3c839
khosit-apctn <khosit.apctn@gmail.com>
2026-09-05 11:27:58 +0700

    feat: initial spring boot project

---
+++ src/test/java/com/shortener/url_shortener_service/UrlShortenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortener.url_shortener_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
